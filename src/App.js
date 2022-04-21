353694e4c346a663cef97a161e3336e53372290d
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-04-21 20:29:52 -0300

    profile class completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './components/profile/profile.js';
 
 function App() {
   return (
+    
     <div className="App">
+      <Profile user={
+          {
+            name: 'Juan',
+            username: 'juanito',
+            email: 'adad@gmail.com',
+            photo: 'https://picsum.photos/200/300',
+        }} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
