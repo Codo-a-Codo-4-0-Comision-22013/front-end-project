71a05cd52500f327c1091fdb6aa40440a8fa14ce
danielpalli <feerhh91@gmail.com>
2022-05-08 19:30:23 -0300

    Base formulario de registro y login

--- netplix-app/src/components/auth/LoginForm.jsx
+++ netplix-app/src/components/auth/LoginForm.jsx
@@ -1,6 +1,18 @@
 export const LoginForm = () => {
   return (
     <>
+      <h1>Inicia sesión</h1>
+      <form action="">
+        <label htmlFor="">Email</label>
+        <input type="text" />
+        <label htmlFor="">Contraseña</label>
+        <input type="text" />
+        <button>Iniciar sesión</button>
+        <input type="checkbox" name="" id="" />
+        <small>¿Necesitas ayuda?</small>
+
+        <span>¿Primera vez en Netplix?</span> <small>Suscríbete ahora</small>
+      </form>
 
     </>
   );
--- netplix-app/src/components/auth/RegisterForm.jsx
+++ netplix-app/src/components/auth/RegisterForm.jsx
@@ -1,7 +1,25 @@
 export const RegisterForm = () => {
   return (
     <>
+      <h1>Formulario de registro:</h1>
+      <form action="">
+        <label htmlFor="">Nombre</label>
+        <input type="text" />
+        <label htmlFor="">Apellido</label>
+        <input type="text" />
+        <label htmlFor="">Email</label>
+        <input type="text" />
+        <label htmlFor="">Confirmar la dirección de email</label>
+        <input type="text" />
+        <label htmlFor="">Elige una contraseña(de 4 a 10 caracteres)</label>
+        <input type="text" />
+        <label htmlFor="">Confirma la contraseña</label>
+        <input type="text" />
+        <button>Registrarse</button>
+        <input type="checkbox" name="" id="" />
 
+        <span>¿Ya tienes una cuenta?</span> <small>Inicie sesión</small>
+      </form>
     </>
   );
 };
