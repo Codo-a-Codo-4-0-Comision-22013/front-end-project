f7b95598090982e2c5d83d0a45e7fcb918956450
danielpalli <feerhh91@gmail.com>
2022-05-14 23:31:27 -0300

    Update controllers

---
+++ netplix-back/src/controllers/usuarioController.js
@@ -0,0 +1,25 @@
+// Controlador de obtener un usuario
+const obtenerUsuarios = async (req, res) => {
+  res.json({
+    ok: true,
+  });
+};
+
+const crearUsuario = async (req, res) => {
+  res.json({
+    ok: true,
+  });
+};
+
+const actualizarUsuario = async (req, res) => {
+  res.json({
+    ok: true,
+  });
+};
+
+const eliminarUsuario = async (req, res) => {
+  res.json({
+    ok: true,
+  });
+};
+export { obtenerUsuarios, crearUsuario, actualizarUsuario, eliminarUsuario };
