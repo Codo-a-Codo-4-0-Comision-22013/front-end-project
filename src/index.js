3c62de99d64cc2285b0d830a9d9483243f4cc7e1
actassi <actassi@gmail.com>
2022-05-18 09:50:00 -0300

    limpieza de código

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './pages/login';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
     </React.StrictMode>
 );
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,60 +1,9 @@
-// import logo from './DATA/logo-gif3.gif';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <body>
-//       <div className="App">
-//         <div className="App-body">
-//           <h1>Bienvenido a PronQatar 2022</h1> 
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <form className='formulario'>
-//             <input className='usuario'
-//             type="mail"
-//             name="mail"
-//             placeholder='Ingrese su usuario/email' />
-//             <input className='contraseña'
-//             type="password"
-//             name="contraseña"
-//             placeholder='Ingrese su contraseña'
-//               />
-//             <input className='boton' 
-//             type="submit" value="Ingresar" />
-//           </form>
-//         </div>    
-//       </div>
-//     </body>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from 'react'
-import { Grid, Container, Paper, Typography, TextField, Button, CssBaseline } from '@material-ui/core'
+import { Grid, Container, Paper, TextField, Button, CssBaseline } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import logo from './assets/images/qatar-world-cup-2022-2.jpg'
-import { createTheme } from '@material-ui/core/styles';
+import logo from '../assets/images/qatar-world-cup-2022-2.jpg'
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       // light: will be calculated from palette.primary.main,
-//       main: '#ff4400',
-//       // dark: will be calculated from palette.primary.main,
-//       // contrastText: will be calculated to contrast with palette.primary.main
-//     },
-//     secondary: {
-//       light: '#0066ff',
-//       main: '#0044ff',
-//       // dark: will be calculated from palette.secondary.main,
-//       contrastText: '#ffcc00',
-//     },
-//     // Used by `getContrastText()` to maximize the contrast between
-//     // the background and the text.
-//     tonalOffset: 0.2,
-//   },
-// });
-    
+   
 
 const useStyles = makeStyles(theme => ({
 
@@ -88,12 +37,6 @@ const useStyles = makeStyles(theme => ({
       },
     },
 		
-		// backgroundImage: `url(${logo})`,
-		// backgroundRepeat: 'no-repeat',
-		// backgroundSize: 'cover',
-		// backgroundPosition: 'center',
-		// height: '100vh',
-    
 	},
 	container: {	
 		
@@ -114,16 +57,7 @@ const useStyles = makeStyles(theme => ({
 		backgroundRepeat: 'no-repeat',
 		backgroundSize: 'contain',
 		marginTop: theme.spacing(20)
-		// height: '50vh',
-		
-    //  	[theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
-	// 		marginTop: 0,
-	// 		width: '100%',
-	// 		height: '100%'
-	// },
-	// avatar: {
-	// 	margin: theme.spacing(1),
-	// 	backgroundColor: theme.palette.primary.main
+	
 	},	
 	form: {
 		
@@ -137,7 +71,7 @@ const useStyles = makeStyles(theme => ({
 	}
 }));
 
-const App = () => {
+const Login = () => {
 	const [body, setBody] = useState({ nickname: '', password: '' })
 	const classes = useStyles()
 
@@ -158,11 +92,7 @@ const App = () => {
 			<CssBaseline />
 			<Container component={Paper} elevation={0} maxWidth='xs' className={classes.container}>
 				<div className={classes.div}>
-					{/* <Avatar className={classes.avatar}>
-						<LockOutlinedIcon />
-					{/* </Avatar> */}
-				{/* <Typography component='h1' variant='h5'>Sign In</Typography>	 */} 
-
+					
 				
 					<form className={classes.form}>
 						<TextField
@@ -203,4 +133,4 @@ const App = () => {
 	)
 }
 
-export default App
+export default Login;
