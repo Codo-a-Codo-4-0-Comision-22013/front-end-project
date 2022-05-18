c1d85fcd2916078d330aa8a788f030a2cfc3071c
actassi <actassi@gmail.com>
2022-05-18 17:09:54 -0300

    limpieza de código

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './pages/login';
+import Login from './components/login';
 import reportWebVitals from './reportWebVitals';
 
 
