d37c633d942d06b00dc09b9d5d11718904ac3230
actassi <actassi@gmail.com>
2022-05-18 09:49:39 -0300

    limpieza de código

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Login from './pages/login';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Login />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
