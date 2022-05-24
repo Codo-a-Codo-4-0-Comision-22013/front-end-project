fe3b104922dde27e87600ba1a22efa795e5e6998
Sebastian Eugenio <sebas-cs1.6@hotmail.com>
2022-05-23 21:00:57 -0300

    add context and btn for DarkMode

---
+++ src/components/ButtonColorMode.js
@@ -0,0 +1,18 @@
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { useContext } from 'react';
+import { ColorModeContext } from '../context/ColorModeContext';
+
+export default function ButtonColorMode() {
+
+	const { mode, toggleColorMode } = useContext(ColorModeContext);
+	return (
+		<>
+			<IconButton onClick={toggleColorMode} color="inherit">
+				{mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+			</IconButton>
+		</>
+	);
+}
+
