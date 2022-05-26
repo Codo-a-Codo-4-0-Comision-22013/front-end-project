2ea1aee039641c3fafdee87687d763f667cb932e
iara baya <iarabayav@gmail.com>
2022-05-26 18:51:33 -0300

    Layout component created and nesting homePage routes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AuthPage from './pages/AuthPage/AuthPage';
 import HomePage from './pages/HomePage/HomePage';
+import Layout from './containers/Layout/Layout';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <div className="body-bg min-h-screen pt-12 md:pt-20 pb-2 px-2"> */}
-      <Routes>
-        <Route index element={<HomePage/>}/>
-        <Route path='/auth' element={<AuthPage/>}/>
-      </Routes>
+        <Routes>
+          <Route element={<Layout/>}>
+              <Route index element={<HomePage/>}/>
+          </Route>
+          <Route path='/auth' element={<AuthPage/>}/>
+        </Routes>
     </BrowserRouter>    
   );
 }
