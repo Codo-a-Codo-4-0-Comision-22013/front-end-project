6e6e9c5c01b8a0889dc8bd82778d66305f5e7438
iara baya <iarabayav@gmail.com>
2022-05-26 18:43:10 -0300

    react-router-dom created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import './App.css';
-// import Profile from './components/profile/ProfileClass';
-// import AuthPage from './pages/AuthPage/AuthPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AuthPage from './pages/AuthPage/AuthPage';
 import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
-    
-    <div className="body-bg min-h-screen pt-12 md:pt-20 pb-2 px-2">
-
-      {/* <Profile user={
-          {
-            name: 'Juan',
-            username: 'juanito',
-            email: 'adad@gmail.com',
-            photo: 'https://picsum.photos/200/300',
-        }} /> */}
-        {/* <AuthPage/> */}
-
-        <HomePage/>
-
-    </div>
+    <BrowserRouter>
+      {/* <div className="body-bg min-h-screen pt-12 md:pt-20 pb-2 px-2"> */}
+      <Routes>
+        <Route index element={<HomePage/>}/>
+        <Route path='/auth' element={<AuthPage/>}/>
+      </Routes>
+    </BrowserRouter>    
   );
 }
 
