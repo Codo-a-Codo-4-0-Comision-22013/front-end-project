fdd6e101eefc0d40bb521513872f0a16c45bb895
Martin Jusbicher <jusbichermartin@gmail.com>
2022-05-30 19:36:39 -0300

    navbar y carousel

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Profile from './components/profile/profile.js';
-import Login from './components/login/login.js';
+import Profile from './components/Profile/Profile.jsx';
+import Login from './components/Login/Login.jsx';
+import Appbar from './components/Navbar/Navbar';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import CarouselItem from './components/Carousel/Carousel';
 
 function App() {
   return (
-    
     <div className="App">
-      <Login></Login>
-      <span>Hola Esto es otro componente</span>
-      <Profile user={
-          {
-            name: 'Juan',
-            username: 'juanito',
-            email: 'adad@gmail.com',
-            photo: 'https://picsum.photos/200/300',
-        }} />
+      <BrowserRouter>
+      <Appbar />
+        <Routes>
+          <Route path="" element={<CarouselItem/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
