318f29f7561b6ef19c47d96bcb995b30db7c1c8e
Sebastian Eugenio <sebas-cs1.6@hotmail.com>
2022-06-01 23:54:17 -0300

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./components/login";
 import { Register } from "./components/register";
-<<<<<<< HEAD
 import NavBar from "./components/navBar";
 import { ProtectedRoute } from "./components/protectedRoute";
 
 import { AuthProvider } from "./context/authContext";
 import { ColorModeContextProvider } from "./context/colorModeContext";
 import Home from "./pages/home/home";
-=======
-import { Home } from "./components/home";
-import NavBar from "./components/navBar";
-import { ProtectedRoute } from "./components/protectedRoute";
-import { AuthProvider } from "./context/authContext";
-import { ColorModeContextProvider } from "./context/colorModeContext";
-import {ThemeProvider, createTheme} from "@material-ui/core/styles/index";
->>>>>>> origin/main
 
 
 function App() {
@@ -24,19 +15,18 @@ function App() {
 		palette: {
 			primary: {
 				main: '#9a0e31',
-				
-				
-	},
-			secondary: {
-			main: '#797979',
 
-	},
-	},
+
+			},
+			secondary: {
+				main: '#797979',
+
+			},
+		},
 	});
 
 
 	return (
-<<<<<<< HEAD
 		<ColorModeContextProvider>
 			<div className="app-container">
 				<AuthProvider>
@@ -60,33 +50,6 @@ function App() {
 				</AuthProvider>
 			</div>
 		</ColorModeContextProvider>
-=======
-		<ThemeProvider theme={theme}>
-			<ColorModeContextProvider>
-				<div className="app-container">
-					<AuthProvider>
-						<div className="sidebar-container">
-							<NavBar name="loco gatti" avatar="imgs/profile_picture.jpg" />
-						</div>
-						<div className="body-container">
-							<Routes>
-								<Route path="/login" element={<Login />} />
-								<Route
-									path="/"
-									element={
-										<ProtectedRoute>
-											<Home />
-										</ProtectedRoute>
-									}
-								/>
-								<Route path="/register" element={<Register />} />
-							</Routes>
-						</div>
-					</AuthProvider>
-				</div>
-			</ColorModeContextProvider>
-		</ThemeProvider>
->>>>>>> origin/main
 	);
 }
 
