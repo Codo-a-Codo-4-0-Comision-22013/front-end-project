6f324cd3d036d2cbef9b949cc94d3b30e75cb81b
Martin Jusbicher <jusbichermartin@gmail.com>
2022-06-03 13:35:23 -0300

    changes

---
+++ src/language.js
@@ -0,0 +1,28 @@
+export const languages = {
+  en: {
+    LOGIN: {
+      HEADING_LOGIN: "Login",
+      INPUT_PLACEHOLDER_EMAIL: "Write Your Email Here",
+      INPUT_PLACEHOLDER_PASS: "Write Your Password",
+    },
+
+    CONTACT: {
+      HEADING_CONTACT: "Contact Us",
+      INPUT_PLACEHOLDER_EMAIL: "Write Your Email Here",
+      INPUT_PLACEHOLDER_NAME: "Write Your Name",
+      INPUT_PLACEHOLDER_MESSAGE: "Write Your Message",
+    },
+    HOME: {
+      HEADING_WELCOME: "Welcome ",
+      MESSAGE: "This is the demo of string resources.",
+    },
+    NAV: {
+      HEADING_NAV: "Navigation",
+      START: "Initial",
+      MOVIE: "Movies",
+      SERIES: "Series",
+      LIST: "List",
+    },
+  },
+  es: {},
+};
