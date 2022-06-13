3005aa5d8707d77cde03b9642778720d1c27e745
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-06-13 20:30:44 -0300

    New component created...

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Router from './routes';
 import ThemeProvider from './components/navbar/themeProvider';
+import Contact from './components/contact/contact';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           <Router  />
           <span>Hola Esto es otro componente</span>
         </div>
+        <Contact></Contact>
       </ThemeProvider>
   );
 }
---
+++ src/components/contact/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        quidem, quisquam.
+        </p>
+      </div>
+  )};
+
+
+  export default Contact;
