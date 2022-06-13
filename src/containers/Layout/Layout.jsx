3c427eb04694f311dac83a8d25cf22a4d5ccf613
iara baya <iarabayav@gmail.com>
2022-06-13 18:32:29 -0300

    footer and header components

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from '../../components/footer/Footer'
+import Header from '../../components/header/Header'
 
 const Layout = ({children}) => {
   return (
     <>
-      <header>This is a header</header>
+      <Header/>
       
       <main className="body-bg min-h-screen pt-12 md:pt-20 pb-2 px-2">
        <Outlet/>
       </main>
 
-      <footer>This is the footer</footer>
+      <Footer/>
     </>
   )
 }
