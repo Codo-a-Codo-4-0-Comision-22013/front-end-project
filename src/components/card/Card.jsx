73a65484647b66ed116c3a91a635ad7fcd660ead
Nathan <jhond999@gmail.com>
2022-06-24 22:02:48 -0300

    update Card diseño

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -14,18 +14,19 @@ const Card = ({pokemon, url}) => {
         getPokemonData();
     }, [])
 
-    if(pokemonData){        
+    if(pokemonData){     
+        {console.log("Datos pokemon:",pokemonData.id)}   
         return (
-            <div className='max-w-sm rounded overflow-hidden flex flex-col justify-center items-center shadow-lg m-2 py-2'>
-                <img className="w-full" src={pokemonData.sprites.front_default} alt="pokemon"></img>
-                <div className="px-6 py-4">
+            <div className='shadow hover:shadow-2xl max-w-sm rounded overflow-hidden flex flex-col justify-center items-center shadow-lg m-2 px-0.5 py-2 border border-gray-300'>
+                <img className="w-full w-72 h-72" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonData.id}.svg`} alt="pokemon"></img>
+                <div className="px-6 py-4 text-center">
                     <div className="font-bold text-xl mb-2">{pokemon}</div>
-                    <p className="text-gray-700 text-base">
+                    <p className="text-lime-600 text-base italic">
                         {pokemonData.types[0].type.name}
                     </p>
                 </div>
 
-                <div className="px-6 pt-4 pb-2">
+                <div className="flex items-center px-6 pt-4 pb-2">
                     {pokemonData.types.map((e) => (<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{e.type.name}</span>) )}
                 </div>
 
