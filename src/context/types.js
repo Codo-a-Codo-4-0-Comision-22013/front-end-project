ad3a3c838f069f11465813658fa616275484c690
iara baya <iarabayav@gmail.com>
2022-06-26 23:39:56 -0300

    context for fetching pokemons applied

---
+++ src/context/types.js
@@ -0,0 +1,3 @@
+export const GET_POKEMON_LIST = 'GET_POKEMON_LIST'
+export const GET_POKEMON = 'GET_POKEMON'
+export const ADD_POKEMON = 'ADD_POKEMON'
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,18 @@
-import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Card from '../../components/card/Card'
+import GeneralContext from '../../context/GeneralContext'
 
 const HomePage = () => {
-
-  const [pokemonList, setPokemonList] = useState([]);
-
-
-  const getPokemons = async () => {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=20');
-    const data = await res.json();
-    setPokemonList(data.results);
-  }
+	const { getPokemonList, pokemons } = useContext(GeneralContext)
 
   useEffect(()=>{
-    getPokemons();
+		getPokemonList();
   },[])
 
-  if(pokemonList.length > 1){
-    return (
-      <div className='grid gap-4 grid-cols-1 place-content-center md:md:grid-cols-3 xl:grid-cols-6 '>
-        {pokemonList.map(pokemon => <Card key={pokemon.url} pokemon={pokemon.name} url={pokemon.url}/>)}
+  if( pokemons.length > 1){
+		return (
+			<div className='grid gap-4 grid-cols-1 place-content-center md:md:grid-cols-3 xl:grid-cols-6 '>
+        {pokemons.map(pokemon => <Card key={pokemon.url} url={pokemon.url}/>)}
       </div>
     )
   }else{ 
