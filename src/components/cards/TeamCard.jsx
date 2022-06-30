d7e21e05e3b8527cfa581f66cde5b7fa28e14ce1
iara baya <iarabayav@gmail.com>
2022-06-30 18:58:04 -0300

    remove button without function

--- src/components/cards/TeamCard.jsx
+++ src/components/cards/TeamCard.jsx
@@ -18,7 +18,7 @@ const TeamCard = ({name}) => {
     if(pokemon){     
         return (
             <div className='shadow hover:shadow-2xl max-w-sm rounded overflow-hidden flex flex-col justify-center items-center shadow-lg m-2 px-0.5 py-2 border border-gray-300'>
-                <img className="w-full w-72 h-72" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt="pokemon"></img>
+                <img className="w-full w-40 h-40" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt="pokemon"></img>
                 <div className="px-6 py-4 text-center">
                     <div className="font-bold text-xl mb-2">{pokemon.name}</div>
                     <p className="text-lime-600 text-base italic">
@@ -31,8 +31,12 @@ const TeamCard = ({name}) => {
                 </div>
 
 								<ul>
-									{pokemon.stats.map( (e, index) => <li key={index}><span className='bold'>{e.stat.name}</span> {e.base_stat}</li>)}
+									{pokemon.stats.map( (e, index) => <li key={index}><span className='font-bold'>{e.stat.name}</span> {e.base_stat}</li>)}
 								</ul>
+
+								<button onClick={() => console.log('eliminado') } className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Remove from team
+                </button>
             </div>)
     }else {
         return(<p>Cargando...</p>)
