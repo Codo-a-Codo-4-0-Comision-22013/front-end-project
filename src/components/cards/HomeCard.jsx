9920b3c56671c5caf0d7f6c8025f76699861b90d
MiaFate <miapurplefroot@gmail.com>
2022-06-30 20:38:50 -0300

    fix/teamAndProfileGrid

--- src/components/cards/HomeCard.jsx
+++ src/components/cards/HomeCard.jsx
@@ -1,21 +1,21 @@
 import React, { useContext, useState, useEffect } from 'react'
 import GeneralContext from '../../context/GeneralContext';
 
-const HomeCard = ({name}) => {
+const HomeCard = ({ name }) => {
     const [pokemon, setPokemon] = useState();
-		const { getPokemon, addToTeam }= useContext(GeneralContext)
+    const { getPokemon, addToTeam } = useContext(GeneralContext)
 
     const getPokemonData = async () => {
-				const url = `https://pokeapi.co/api/v2/pokemon/${name}/`;
+        const url = `https://pokeapi.co/api/v2/pokemon/${name}/`;
         const data = await getPokemon(url);
         setPokemon(data);
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getPokemonData();
     }, [])
 
-    if(pokemon){     
+    if (pokemon) {
         return (
             <div className='shadow hover:shadow-2xl max-w-sm rounded overflow-hidden flex flex-col justify-center items-center shadow-lg m-2 px-0.5 py-2 border border-gray-300'>
                 <img className="w-full w-72 h-72" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt="pokemon"></img>
@@ -30,12 +30,12 @@ const HomeCard = ({name}) => {
                     {/* {pokemon.types.map((e) => (<span key={e.type.name} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{e.type.name}</span>) )} */}
                 </div>
 
-                <button onClick={() => addToTeam(pokemon) } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <button onClick={() => addToTeam(pokemon)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
                     Add to my team
                 </button>
             </div>)
-    }else {
-        return(<p>Cargando...</p>)
+    } else {
+        return (<p>Cargando...</p>)
     }
 
 }
--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -6,19 +6,19 @@ const TeamPage = () => {
 
 	const { team } = useContext(GeneralContext);
 
-	if(team.length >= 1){
+	if (team.length >= 1) {
 		return (
 			<div className='max-w-7xl min-h-full flex mx-auto p-1 md:p-12 rounded-xl shadow-2xl items-center justify-center'>
-				<div className='grid grid-rows-2 grid-flow-col gap-2'>
-						{
-							team.map( (pokemon, index) => (<TeamCard key={index} name={pokemon.name}/>))
-						}
+				<div className='grid gap-4 grid-cols-1 place-content-center md:md:grid-cols-3 xl:grid-cols-3'>
+					{
+						team.map((pokemon, index) => (<TeamCard key={index} name={pokemon.name} />))
+					}
 				</div>
 			</div>
 		)
-	}else{
+	} else {
 
-		return(
+		return (
 			<div className='max-w-3xl min-h-full flex flex-col mx-auto p-6 md:p-12 rounded-xl shadow-2xl items-center'>
 				No hay pokemones en tu equipo
 			</div>
